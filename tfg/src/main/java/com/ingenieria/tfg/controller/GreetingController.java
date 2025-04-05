95ba6014f5e17de8571cc18ae4c5cb0493038ba3
AlmaGomez2807 <alma280709@gmail.com>
2025-04-05 11:51:52 +0200

    Prueba1

---
+++ tfg/src/main/java/com/ingenieria/tfg/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.ingenieria.tfg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "principal";
+    }
+}
