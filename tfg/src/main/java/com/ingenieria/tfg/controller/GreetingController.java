1e3f90f0fb501edbc8af5d749a1141fc398bd7ac
AlmaGomez2807 <alma280709@gmail.com>
2025-07-12 10:22:01 +0200

    vjulio

--- tfg/src/main/java/com/ingenieria/tfg/controller/GreetingController.java
+++ tfg/src/main/java/com/ingenieria/tfg/controller/GreetingController.java
@@ -8,6 +8,36 @@ public class GreetingController {
 
     @RequestMapping("/")
     public String greeting() {
+        return "principal";
+    }
+
+    @RequestMapping("/principalJuegos")
+    public String greeting2() {
+        return "principalJuegos";
+    }
+
+    @RequestMapping("/coordinate")
+    public String greeting3() {
         return "coordinate";
+    }
+
+    @RequestMapping("/headingconflicts")
+    public String greeting4() {
+        return "headingconflicts";
+    }
+
+    @RequestMapping("/radarcontrol")
+    public String greeting5() {
+        return "radarcontrol";
+    }
+
+    @RequestMapping("/multitask")
+    public String greeting6() {
+        return "multitask";
+    }
+
+    @RequestMapping("/figures")
+    public String greeting7() {
+        return "figures";
     }
 }
